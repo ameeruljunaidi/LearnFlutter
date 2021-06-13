dd687a990b7f81389a95ec48b5fe7ec5e127cdc8
Ameerul Junaidi <ameerul.junaidi@gmail.com>
2021-06-13 16:16:43 -0400

    updated udemy learning

--- udemy_flutter/flutter_basics/lib/result.dart
+++ udemy_flutter/flutter_basics/lib/result.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 // ignore: use_key_in_widget_constructors
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
   // ignore: use_key_in_widget_constructors
-  const Result(this.resultScore);
+  const Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
     if (resultScore <= 20) {
-      resultText = "You suck!";
+      resultText = "You suck bitch!";
     } else if (resultScore <= 25) {
       resultText = "You're aight...";
     } else if (resultScore <= 30) {
@@ -24,12 +25,21 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: const TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: resetHandler,
+            child: const Text("Restart Quiz!"),
+          )
+        ],
       ),
     );
   }
