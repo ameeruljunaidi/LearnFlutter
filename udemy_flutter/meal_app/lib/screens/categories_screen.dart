f9599c6457d09f94f295c52039532e14c1696f1d
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-07 00:48:53 -0400

    Added the new meal app - Focused on making screen and - Navigating across screens - Next session will focus on sidebar

---
+++ udemy_flutter/meal_app/lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/data/dummy_data.dart';
+import 'package:flutter_complete_guide/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meal App"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
