b580238f1f9cce29a031dec86a492850c25d1c8d
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-08 13:07:03 -0400

    Listening in different places and ways - Add it to the product detail screen - Pull the title from the provider - Move the filtering for the id into the provider.dart file - You can use a method to do this - Make sure all the logics are in the provider class rather than on the widget itself - Can set listen to false if you only want the data to be fetched once

--- udemy_flutter/shop_app/lib/providers/product_providers.dart
+++ udemy_flutter/shop_app/lib/providers/product_providers.dart
@@ -11,6 +11,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((Product prod) => prod.id == id);
+  }
+
   void addProduct() {
     // _items.add(value);
     notifyListeners();
--- udemy_flutter/shop_app/lib/screens/product_detail_screen.dart
+++ udemy_flutter/shop_app/lib/screens/product_detail_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/product_providers.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const String routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+    final String productId =
+        ModalRoute.of(context).settings.arguments as String;
+    final Product loadedProduct =
+        Provider.of<Products>(context).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
