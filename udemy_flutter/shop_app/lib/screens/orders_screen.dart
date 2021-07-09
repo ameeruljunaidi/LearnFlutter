05352b377e9db1bb054bbf9f422e9f0c2504474e
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-08 21:08:35 -0400

    Added and orders screen

---
+++ udemy_flutter/shop_app/lib/screens/orders_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/order.dart' show Orders;
+import 'package:shop_app/widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Orders orderData = Provider.of<Orders>(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Orders'),
+        ),
+        body: ListView.builder(
+          itemBuilder: (BuildContext ctx, int i) =>
+              OrderItem(orderData.orders[i]),
+          itemCount: orderData.orders.length,
+        ));
+  }
+}
---
+++ udemy_flutter/shop_app/lib/widgets/order_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shop_app/providers/order.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  OrderItem(this.order);
+
+  final ord.OrderItem order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+            ),
+            trailing: const IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
