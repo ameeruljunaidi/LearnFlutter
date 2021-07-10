46200f5fecef2eafab7ed30ae7817433e22f4bd0
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-10 14:21:09 -0400

    Storing orders on firebase

--- udemy_flutter/shop_app/lib/providers/order.dart
+++ udemy_flutter/shop_app/lib/providers/order.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrderItem {
   OrderItem({
@@ -24,11 +27,36 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final Uri url = Uri.https(
+      'shop-app-b0190-default-rtdb.firebaseio.com',
+      '/products.json',
+    );
+
+    final DateTime timeStamp = DateTime.now();
+
+    // ignore: always_specify_types
+    final http.Response response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timeStamp.toIso8601String(),
+        // ignore: always_specify_types
+        'products': cartProducts
+            .map((CartItem cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'].toString(),
         amount: total,
         products: cartProducts,
         dateTime: DateTime.now(),
